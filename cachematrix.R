a50e9621e2bceb4220f37830a8ab35692345cd20
mmdelreal <moises.delreal@gmail.com>
2015-10-23 23:53:52 -0500

    Final change
    R code for cachematrix is now complete
    MdR

--- cachematrix.R
+++ cachematrix.R
@@ -1,12 +1,26 @@
 ###Moises M. del Real
-## Put comments here that give an overall description of what your functions do
+#The following 2 functions create a "matrix object", invert it, 
+#cache the inverted matrix, and then computes the inverse of the matrix in
+#question, unless it has already been calculated and is in the cache (via
+#the "matrix object")
+
 
 ## "The following function makes a special "matrix object" that caches its own inverse
 
 makeCacheMatrix <- function(x=matrix()) {
-
-  matobj<-x
-  invers<<-solve(matobj)
+  
+  mobj <- NULL
+  
+  set <- function(y) {
+    x <<- y
+    mobj <<- NULL
+  }
+  get <- function() x
+  setinvers <- function(solve) mobj <<- solve
+  getinvers <- function() mobj
+  list(set = set, get = get,
+       setinvers = setinvers,
+       getinvers = getinvers)
 }
 
 
@@ -15,42 +29,27 @@ makeCacheMatrix <- function(x=matrix()) {
 #the matrix 'x'
 
 cacheSolve <- function(x, ...) {
-        
-  invers<-invers
   
-  ## Checks to see if 'invers' value is NULL
-  ## If 'invers' is not NULL, then return the original inverse of 'x'
+  mobj<-x$getinvers()
   
-  if(!is.null(invers)) {
+  ## Checks to see if 'mobj' value is NULL
+  
+  if(!is.null(mobj)) {
     
-    message("getting cached data")
-    round(invers,4)
+    # If 'mobj' is not NULL, then return the original, cached inverse of 'x'  
+    
+    message("Obtaining cached data... ^_^")
+    mobj
+    
+  }
   
-  } else {
   
-  ## If 'invers' is NULL, then return a matrix that is the inverse of 'x'
-  matobj<-x
-  invers2<-solve(matobj)
+  ## If 'mobj' is NULL, then return a matrix that is the inverse of 'x'
+  
+  inversdata <- x$get()
+  mobj <- solve(inversdata, ...)
+  x$setinvers(mobj)
   
   ##final product :)
-  round(invers2,4)
-  
-  }
+  mobj
 }
-
-
-
-
-#Matrix inversion is usually a costly computation and there may be some benefit to caching the inverse of a matrix rather than computing it repeatedly (there are also alternatives to matrix inversion that we will not discuss here). Your assignment is to write a pair of functions that cache the inverse of a matrix.
-
-#Write the following functions:
-  
-#makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
-
-#cacheSolve: This function computes the inverse of the special "matrix" returned by 
-#makeCacheMatrix above. If the inverse has already been calculated 
-#(and the matrix has not changed), then cacheSolve should retrieve the inverse from the cache.
-
-#Computing the inverse of a square matrix can be done with the solve function in R. For example, 
-#if X is a square invertible matrix, then solve(X) returns its inverse.For this assignment, 
-#assume that the matrix supplied is always invertible.
